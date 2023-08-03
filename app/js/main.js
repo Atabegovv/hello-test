04d9c55cc3b0a39d6d21127cb22853170c5af3b4
Atabegovv <atabegov98@mail.ru>
2023-08-03 12:33:21 +0300

    section feedback and footer desktop

--- app/js/main.js
+++ app/js/main.js
@@ -1,1 +1,12 @@
 'use strict';
+
+const swiper = new Swiper('.swiper', {
+	navigation: {
+		prevEl: '.swiper-btn-prev',
+		nextEl: '.swiper-btn-next',
+	},
+	grabCursor: true,
+	speed: 1000,
+	effect: 'flip',
+	loop: true
+});
